bcbec95d55380d5872e3f217bc5f2256ca1a8e15
Gareth McMullin <gareth@blacksphere.co.nz>
2019-02-20 21:57:25 +1300

    cortexa: Set PMC bits in DBGBCR

--- src/target/cortexa.c
+++ src/target/cortexa.c
@@ -130,6 +130,7 @@ struct cortexa_priv {
 #define DBGBCR_BAS_ANY           (0xf << 5)
 #define DBGBCR_BAS_LOW_HW        (0x3 << 5)
 #define DBGBCR_BAS_HIGH_HW       (0xc << 5)
+#define DBGBCR_PMC_ANY           (0b11 << 1)
 #define DBGBCR_EN                (1 << 0)
 
 #define DBGLAR                   (1004)
@@ -720,7 +721,7 @@ void cortexa_halt_resume(target *t, bool step)
 		/* Set match any breakpoint */
 		apb_write(t, DBGBVR(0), priv->reg_cache.r[15] & ~3);
 		apb_write(t, DBGBCR(0), DBGBCR_INST_MISMATCH | bas |
-		                             DBGBCR_EN);
+		                        DBGBCR_PMC_ANY | DBGBCR_EN);
 	} else {
 		apb_write(t, DBGBVR(0), priv->bvr0);
 		apb_write(t, DBGBCR(0), priv->bcr0);
@@ -802,7 +803,8 @@ static int cortexa_breakwatch_set(target *t, struct breakwatch *bw)
 		priv->hw_breakpoint_mask |= (1 << i);
 
 		uint32_t addr = va_to_pa(t, bw->addr);
-		uint32_t bcr =  bp_bas(addr, bw->size) | DBGBCR_EN;
+		uint32_t bcr =  bp_bas(addr, bw->size) |
+		                DBGBCR_PMC_ANY | DBGBCR_EN;
 		apb_write(t, DBGBVR(i), addr & ~3);
 		apb_write(t, DBGBCR(i), bcr);
 		if (i == 0) {
